637e53ac632d780adbb7ba162c03010820616bb5
sudhir singh <118928008+sudhirmavai@users.noreply.github.com>
2023-06-17 10:14:46 +0530

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+def space():
+    st.markdown("<br>", unsafe_allow_html=True)
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="😎",
+)
+
+
+st.write("# Heya Everyone ! 👋")
+
+st.markdown("# :red[Project mentored byNIT Raipur ]")
+st.subheader(" :blue[Aspiring Data Scientist]")
+space()
+
+
+st.markdown(
+    """
+    #### We specialize in Data Science , data analytics and Machine Learning and our passion is to take a cup of coffee and start finding some meaningful insight in messy data and build some great model...
+    """
+)
+
+space()
+
+st.write("#### Connect me on [LinkedIn](https://www.linkedin.com/)")
+st.write("#### Follow me on [GitHub](https://github.com)")
+st.write("#### Follow me on [Medium](https://medium.com)")
--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# Laptop_price_predictor
-
-End to end machine learning project for laptop price predictor 
+# Flipkart Laptop Price Prediction
